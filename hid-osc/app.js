0fab325b21b59ef36f2a75a6b9c21185f24b6675
Jason Plumb <jason@noisybox.net>
2015-08-08 22:28:47 -0700

    more failed node hid attempts

--- hid-osc/app.js
+++ hid-osc/app.js
@@ -27,7 +27,6 @@ if(!device){
 	throw new Error("Could not find ontrack device in HID devices list");
 }
 console.log("Found device: " + JSON.stringify(device));
-
 device.on('data', function(data){
 	console.log("RECEIVED DATA: " + JSON.stringify(data));
 });
@@ -36,7 +35,32 @@ device.on('error', function(err){
 	console.log("DEVICE ERROR: " + err);
 });
 
-setTimeout(function(){
+device.resume();
+
+// if(device._paused){
+// 	console.log("Device is paused...resuming");
+// 	device.resume();
+// 	console.log("Now device: " + JSON.stringify(device));
+// }
+
+function sendToDevice(){
 	console.log("Sending data to device...");
-	device.write(['P', 'I', 0x00, 0x00, 0x00, 0x00, 0x00]);
-}, 500);
+	// device.write(['P', 'I', 0x00, 0x00, 0x00, 0x00, 0x00]);
+	// device.write(['P', 'B']);
+	device.write([1, 'P', 'B', 0, 0, 0, 0, 0]);
+	setTimeout(sendToDevice, 500);
+}
+// console.log("Sending data to device...");
+// device.write([1, 'P', 'B', 0, 0, 0, 0, 0]);
+
+// setTimeout(function(){ console.log("zzzz");}, 5000);
+
+device.write([1, 'P', 'B', 0, 0, 0, 0, 0]);
+
+var timer = setTimeout(sendToDevice, 500);
+// timer = setTimeout(sendToDevice, 500);
+// sendToDevice();
+// device.read(function(err, data){
+// 	console.log("READ ERR: " + err);
+// 	console.log("READ: " + data);
+// });
